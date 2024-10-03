4561a3388626dc9690808418c39b6188806eab88
ahmed bacha <sam@google.com>
2024-10-03 15:57:17 +0100

    initialisation du projet timesheet-devops

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.GeneratedValue; //dvfkvjobkgtg
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
